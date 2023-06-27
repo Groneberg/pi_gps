16fcf1f7d3347fc5c437614bb25b6bd338b1752a
groneberg <stefan.groneberg@gmx.de>
2023-06-27 11:36:25 +0200

    change gps.py

--- gps.py
+++ gps.py
@@ -1,22 +1,26 @@
-import serial
+import gps
 
-# Serielle Verbindung herstellen
-ser = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1)
+# GPS-Verbindung herstellen
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
 while True:
-    # Daten von der seriellen Schnittstelle lesen
-    line = ser.readline().decode('utf-8').rstrip()
+    try:
+        # Daten von GPS-Modul empfangen
+        report = session.next()
 
-    # Überprüfen, ob die Zeile GPS-Daten enthält
-    if line.startswith('$GNRMC') or line.startswith('$GPRMC'):
-        data = line.split(',')
-        if data[2] == 'V':
-            print("Keine gültigen GPS-Daten verfügbar")
-        else:
-            latitude = float(data[3][:2]) + float(data[3][2:]) / 60.0
-            longitude = float(data[5][:3]) + float(data[5][3:]) / 60.0
-            print("Latitude: ", latitude)
-            print("Longitude: ", longitude)
+        if report['class'] == 'TPV':
+            if hasattr(report, 'lat') and hasattr(report, 'lon'):
+                latitude = report.lat
+                longitude = report.lon
+                print("Latitude: ", latitude)
+                print("Longitude: ", longitude)
 
-# Serielle Verbindung schließen
-ser.close()
+    except KeyboardInterrupt:
+        # Skript bei Tastaturunterbrechung beenden
+        break
+
+    except StopIteration:
+        # GPS-Stream beendet
+        print("GPS-Stream beendet")
+
