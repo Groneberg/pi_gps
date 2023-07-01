96f04e495c94949ae332581313fc62a1cea6bddc
groneberg <stefan.groneberg@gmx.de>
2023-07-01 17:24:51 +0200

    change gps.py

--- gps.py
+++ gps.py
@@ -7,32 +7,32 @@ print(ser)
 # Endlosschleife zum Lesen der GPS-Daten
 while True:
     # Zeile von der seriellen Schnittstelle lesen
-    line = ser.readline().decode('ascii').strip()
+    line = ser.readline().decode('utf-8').strip()
     print(line)
 
-    # Nur die NMEA-Zeilen verarbeiten
-    if line.startswith('$GNGGA'):
-        data = line.split(',')
-
-        # Positionsinformationen extrahieren
-        latitude = data[2]
-        longitude = data[4]
-        altitude = data[9]
-
-        # Ausgabe der GPS-Daten
-        print('Latitude: {}'.format(latitude))
-        print('Longitude: {}'.format(longitude))
-        print('Altitude: {}'.format(altitude))
-    if line.startswith('$GNRMC'):
-        gps_data = line.split(',')
-
-        # Breitengrad und Längengrad extrahieren
-        latitude = gps_data[3]
-        longitude = gps_data[5]
-
-        # Ausgabe der GPS-Daten
-        print('Latitude:', latitude)
-        print('Longitude:', longitude)
-
-# Serielle Verbindung schließen
-ser.close()
+#     # Nur die NMEA-Zeilen verarbeiten
+#     if line.startswith('$GNGGA'):
+#         data = line.split(',')
+#
+#         # Positionsinformationen extrahieren
+#         latitude = data[2]
+#         longitude = data[4]
+#         altitude = data[9]
+#
+#         # Ausgabe der GPS-Daten
+#         print('Latitude: {}'.format(latitude))
+#         print('Longitude: {}'.format(longitude))
+#         print('Altitude: {}'.format(altitude))
+#     if line.startswith('$GNRMC'):
+#         gps_data = line.split(',')
+#
+#         # Breitengrad und Längengrad extrahieren
+#         latitude = gps_data[3]
+#         longitude = gps_data[5]
+#
+#         # Ausgabe der GPS-Daten
+#         print('Latitude:', latitude)
+#         print('Longitude:', longitude)
+#
+# # Serielle Verbindung schließen
+# ser.close()
