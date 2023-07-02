7e7c706faa8241d3a6cbf4b71a8cceb3f8336c53
groneberg <stefan.groneberg@gmx.de>
2023-07-02 14:47:32 +0200

    change gps.py

--- gps.py
+++ gps.py
@@ -1,53 +1,38 @@
 import serial
 
 # Serielle Schnittstelle konfigurieren
-ser1 = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1)
-ser2 = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1)
-ser3 = serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
-print("ser1", ser1)
-print("----")
-print("ser2", ser2)
-print("----")
-print("ser3", ser3)
-print("----")
-
+ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
+print(ser)
 
 # Endlosschleife zum Lesen der GPS-Daten
 while True:
     # Zeile von der seriellen Schnittstelle lesen
-    line1 = ser1.readline().decode('utf-8').strip()
-    print(line1)
-    print("----1")
-    line2 = ser2.readline().decode('utf-8').strip()
-    print(line2)
-    print("----2")
-    line3 = ser3.readline().decode('utf-8').strip()
-    print(line3)
-    print("----3")
+    line = ser.readline().decode('utf-8').strip()
+    print(line)
 
-#     # Nur die NMEA-Zeilen verarbeiten
-#     if line.startswith('$GNGGA'):
-#         data = line.split(',')
-#
-#         # Positionsinformationen extrahieren
-#         latitude = data[2]
-#         longitude = data[4]
-#         altitude = data[9]
-#
-#         # Ausgabe der GPS-Daten
-#         print('Latitude: {}'.format(latitude))
-#         print('Longitude: {}'.format(longitude))
-#         print('Altitude: {}'.format(altitude))
-#     if line.startswith('$GNRMC'):
-#         gps_data = line.split(',')
-#
-#         # Breitengrad und Längengrad extrahieren
-#         latitude = gps_data[3]
-#         longitude = gps_data[5]
-#
-#         # Ausgabe der GPS-Daten
-#         print('Latitude:', latitude)
-#         print('Longitude:', longitude)
-#
-# # Serielle Verbindung schließen
-# ser.close()
+    # Nur die NMEA-Zeilen verarbeiten
+    if line.startswith('$GNGGA'):
+        data = line.split(',')
+        print("data", data)
+        # Positionsinformationen extrahieren
+        latitude = data[2]
+        longitude = data[4]
+        altitude = data[9]
+
+        # Ausgabe der GPS-Daten
+        print('Latitude: {}'.format(latitude))
+        print('Longitude: {}'.format(longitude))
+        print('Altitude: {}'.format(altitude))
+    if line.startswith('$GNRMC'):
+        gps_data = line.split(',')
+        print("gps_data", gps_data)
+        # Breitengrad und Längengrad extrahieren
+        latitude = gps_data[3]
+        longitude = gps_data[5]
+
+        # Ausgabe der GPS-Daten
+        print('Latitude:', latitude)
+        print('Longitude:', longitude)
+
+# Serielle Verbindung schließen
+ser.close()
