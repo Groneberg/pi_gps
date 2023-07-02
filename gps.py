f42842a9931d77d2bc59c3568216fd775237b17d
groneberg <stefan.groneberg@gmx.de>
2023-07-02 15:53:35 +0200

    change gps.py

--- gps.py
+++ gps.py
@@ -6,47 +6,52 @@ print(ser)
 
 # Endlosschleife zum Lesen der GPS-Daten
 while True:
-    # Zeile von der seriellen Schnittstelle lesen
-    line = ser.readline().decode('utf-8').strip()
-    # print(line)
+    try:
+        # Zeile von der seriellen Schnittstelle lesen
+        line = ser.readline().decode('utf-8').strip()
+        # print(line)
 
-    # Nur die NMEA-Zeilen verarbeiten
-    if line.startswith('$GNGGA'):
-        data = line.split(',')
-        print("UTC-Zeit", data[1])
-        print("Breitengrad", data[2])
-        print("Breitengrad-Richtung", data[3])
-        print("Längengrad", data[4])
-        print("Längengrad-Richtung", data[5])
-        print("GPS-Qualitätsindikator", data[6])
-        print("Anzahl der verwendeten Satelliten", data[7])
-        print("HDOP", data[8])
-        print("Höhe über dem Meeresspiegel", data[9])
-        print("Höheneinheit", data[10])
-        print("Geoid-Höhe", data[11])
-        print("Geoid-Höhen-Einheit", data[12])
-        print("Zeit seit der letzten DGPS-Aktualisierung", data[13])
-        print("DGPS-Referenzstation-ID", data[14])
+        # Nur die NMEA-Zeilen verarbeiten
+        if line.startswith('$GNGGA'):
+            GNGGA_data = line.split(',')
+            print("UTC-Zeit:", GNGGA_data[1])
+            print("Breitengrad:", GNGGA_data[2])
+            print("Breitengrad-Richtung:", GNGGA_data[3])
+            print("Längengrad:", GNGGA_data[4])
+            print("Längengrad-Richtung:", GNGGA_data[5])
+            print("GPS-Qualitätsindikator:", GNGGA_data[6])
+            print("Anzahl der verwendeten Satelliten:", GNGGA_data[7])
+            print("HDOP:", GNGGA_data[8])
+            print("Höhe über dem Meeresspiegel:", GNGGA_data[9])
+            print("Höheneinheit:", GNGGA_data[10])
+            print("Geoid-Höhe:", GNGGA_data[11])
+            print("Geoid-Höhen-Einheit:", GNGGA_data[12])
+            print("Zeit seit der letzten DGPS-Aktualisierung:", GNGGA_data[13])
+            print("DGPS-Referenzstation-ID:", GNGGA_data[14])
 
-#         # Positionsinformationen extrahieren
-#         latitude = data[2]
-#         longitude = data[4]
-#         altitude = data[9]
-#
-#         # Ausgabe der GPS-Daten
-#         print('Latitude: {}'.format(latitude))
-#         print('Longitude: {}'.format(longitude))
-#         print('Altitude: {}'.format(altitude))
-#     if line.startswith('$GNRMC'):
-#         gps_data = line.split(',')
-#         print("gps_data", gps_data)
-#         # Breitengrad und Längengrad extrahieren
-#         latitude = gps_data[3]
-#         longitude = gps_data[5]
-#
-#         # Ausgabe der GPS-Daten
-#         print('Latitude:', latitude)
-#         print('Longitude:', longitude)
-#
+    #         # Positionsinformationen extrahieren
+    #         latitude = data[2]
+    #         longitude = data[4]
+    #         altitude = data[9]
+    #
+    #         # Ausgabe der GPS-Daten
+    #         print('Latitude: {}'.format(latitude))
+    #         print('Longitude: {}'.format(longitude))
+    #         print('Altitude: {}'.format(altitude))
+        if line.startswith('$GNRMC'):
+            GNRMC_data = line.split(',')
+            print("Status:", GNRMC_data[3])
+            print("Geschwindigkeit über Grund:", GNRMC_data[9])
+            print("Modus:", GNRMC_data[14])
+    #         # Breitengrad und Längengrad extrahieren
+    #         latitude = gps_data[3]
+    #         longitude = gps_data[5]
+    #
+    #         # Ausgabe der GPS-Daten
+    #         print('Latitude:', latitude)
+    #         print('Longitude:', longitude)
+    except Exception as e:
+        print("Fehler:", str(e))  # Fehlermeldung ausgeben
+
 # # Serielle Verbindung schließen
 # ser.close()
