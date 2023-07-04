0414469dfcd2d5ca981a3e9b7f83186a25d5c116
groneberg <stefan.groneberg@gmx.de>
2023-07-04 10:31:17 +0200

    change gps.py

--- gps.py
+++ gps.py
@@ -1,7 +1,7 @@
 import serial
 
 # Serielle Schnittstelle konfigurieren
-ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
+ser = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1)
 print(ser)
 
 # Endlosschleife zum Lesen der GPS-Daten
